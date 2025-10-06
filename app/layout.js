3ad290a24e801e128a07861eb3a6a2111897757a
Md Khalid Hossen <mdkhalidhossen@hotmail.com>
2025-10-06 21:27:00 +0600

    feat(layout): add global layout with Clerk and UI components
    - Set up global layout with ClerkProvider for authentication
    - Include Header, Footer, and Toaster components for global UI
    - Use Inter font and apply global styles from `globals.css`
    - Set metadata for site title and description

---
+++ app/layout.js
@@ -0,0 +1,32 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Header } from "@/components/header.jsx";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
+import { Footer } from "@/components/footer.jsx";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Flowa",
+  description: "One stop Finance Platform",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${inter.className}`}
+        >
+          <Header />
+          <main className="min-h-screen">
+            {children}
+          </main>
+          <Toaster richColors />
+          <Footer/>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
